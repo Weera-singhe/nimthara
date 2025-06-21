29c3dfcb738d7e5d88514414ce8dd300a5503999
Weera-singhe <kavisandyy@gmail.com>
2025-06-21 15:20:19 +0530

    Update URLs to support production backend

--- client/src/api/urls.js
+++ client/src/api/urls.js
@@ -1,4 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://your-production-url.com";
+  process.env.REACT_APP_API_BASE_URL || "https://nimthara-server.onrender.com";
+
 export const PAPERS_API_URL = `${API_BASE_URL}/papers`;
-export const ADD_PAPER_URL = `${API_BASE_URL}/add_new_paper`;
+export const ADD_PAPER_API_URL = `${API_BASE_URL}/add_new_paper`;
